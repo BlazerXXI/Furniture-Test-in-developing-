35338d274fd3a4cdad35a7c7084225e6e7db0ac5
BlazerXXI <ruslandendik@gmail.com>
2023-08-15 08:35:18 +0300

    fix all

--- js/index.js
+++ js/index.js
@@ -3,9 +3,24 @@ const menuList = document.querySelector(".nav__list");
 let isMenuOpen = false;
 
 menuHamburger.addEventListener("click", () => {
+	toggleMenu();
+});
+
+document.addEventListener("click", () => {
+	if (event && event.target !== menuList) {
+		closeMenu();
+	}
+});
+
+function toggleMenu() {
 	isMenuOpen = !isMenuOpen;
 	menuList.classList.toggle("open");
 	menuHamburger.classList.toggle("menu__hamburger-open");
+}
 
-
-});
+function closeMenu() {
+	isMenuOpen
+		? (isMenuOpen = false)
+		: menuList.classList.remove("open") ||
+		  menuHamburger.classList.remove("menu__hamburger-open");
+}
